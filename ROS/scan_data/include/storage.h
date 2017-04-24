73f341d1b1ea3b06e033b624d1cb966efc8883dd
Sarvesh Srinivasan <sarvesh.srini97@gmail.com>
2017-04-24 02:32:29 +0100

    ROS Node Updated

--- ROS/scan_data/include/storage.h
+++ ROS/scan_data/include/storage.h
@@ -10,5 +10,5 @@ public:
   static vector< pair<double,double> > _points;
   // static void update();
   // static void detection_callback(sensor_msgs::LaserScan::ConstPtr& a);
-  static void print_points(int count);
+  static void print_points(int count,char*);
 };
--- ROS/scan_data/src/scan_data_node.cpp
+++ ROS/scan_data/src/scan_data_node.cpp
@@ -4,30 +4,52 @@
 #include <cstdio>
 
 using namespace std;
-
+int flag;
 vector<pair<double,double> > PointStore::_points;
+char *file_name;
 
-void PointStore::print_points(int count) {
-  for (int i = 0; i < count; i++)
-    printf("Point %d: %lf, %lf\n",i,PointStore::_points[i].first,PointStore::_points[i].second);
+void PointStore::print_points(int count,char* file_name) {
+  FILE* fp = fopen(file_name,"w");
+  for (int i = 0; i < PointStore::_points.size(); i++)
+    fprintf(fp,"%lf, %lf\n",PointStore::_points[i].first,PointStore::_points[i].second);
+  fclose(fp);
 }
 
 void detection_callback(const sensor_msgs::LaserScan::ConstPtr& msg) {
-  const vector<float> input = msg->ranges;
-  PointStore::_points.clear();
-  double j = msg->angle_min;
-  for (int i = 0; i < input.size(); i++) {
-    if (input[i] > msg->range_max)
-      ;
-    else
-      PointStore::_points.push_back(std::make_pair(input[i],j));
-    j+=msg->angle_increment;
+  if (flag) {
+    flag = 0;
+    const vector<float> input = msg->ranges;
+    PointStore::_points.clear();
+    double j = msg->angle_min;
+    cout << msg->angle_max<<endl;
+    cout << (int) ((msg->angle_max - msg->angle_min)/msg->angle_increment) <<input.size()<< endl;
+    for (int i = 0; i < input.size(); i++) {
+      if ((input[i] > msg->range_max)||(input[i] < 0))
+	     ;
+      else {
+        PointStore::_points.push_back(std::make_pair(input[i],j));
+      }
+      j+=msg->angle_increment;
+    }
+    PointStore::print_points(511,file_name);
+    cout << j<<'\t'<<"Finished\n";
   }
-  PointStore::print_points(2);
+  else
+    ;
 }
 
 int main(int argc, char** argv) {
+
+  file_name = (char *)malloc(255*sizeof(char));
+
   ros::init(argc,argv,"scan_data_node");
+  
+  flag = 1;
+
+  if (argc<2) 
+    printf("!!!WARNING: Expected file name!!!\n");
+  else
+    file_name = argv[1];
 
   ros::NodeHandle n;
 
